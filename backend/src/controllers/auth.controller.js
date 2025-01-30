a9ffc73319b3e1852bccb69b7dc0d02f4bf43973
Aditya <singhaditya5096@gmail.com>
2025-01-30 12:05:19 +0530

    chatapp

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -118,6 +118,6 @@ export const checkAuth = (req, res) => {
     res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Serve Error" });
   }
 };
